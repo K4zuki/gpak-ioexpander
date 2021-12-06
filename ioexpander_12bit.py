47669559e2526c9a951ce0b52fe4b59d32ced5f4
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2021-12-06 21:25:38 +0900

    update ioexpander_12bit.py - add Mux class; add main()

--- ioexpander_12bit.py
+++ ioexpander_12bit.py
@@ -67,35 +67,81 @@ class Port:
 
     def write(self, data):
         assert 256 > data >= 0
-        for sft in range(6):
-            self.set_bit(sft, 0x01 & (data >> sft))
+        [self.set_bit(sft, 0x01 & (data >> sft)) for sft in range(6)]
         self.send()
 
     def read(self):
-        data = bus.read_byte_data(self.address, self.read_address)
+        data = self.bus.read_byte_data(self.address, self.read_address)
         for bit in self.bits:
             bit.val = 0x01 & (data >> bit.reading_bit)
         return data
 
 
-with SMBus(1) as bus:
-    port0 = Port(bus, ioexpander_address)
-    port0.read_address = P0_REG
-    port0.bit0 = Bit(2, 0)
-    port0.bit1 = Bit(3, 1)
-    port0.bit2 = Bit(4, 2)
-    port0.bit3 = Bit(5, 3)
-    port0.bit4 = Bit(6, 4)
-    port0.bit5 = Bit(7, 5)
+class Mux:
+    def __init__(self, bus, address):
+        self.address = address
+        self.bus = bus  # type: smbus2.SMBus
 
-    port1 = Port(bus, ioexpander_address)
-    port1.read_address = P1_REG
-    port1.mask = 2
+        self.port0 = Port(self.bus, self.address)
+        self.port0.read_address = P0_REG
+        self.port0.mask = 1
+        self.port0.bit0 = Bit(2, 0)
+        self.port0.bit1 = Bit(3, 1)
+        self.port0.bit2 = Bit(4, 2)
+        self.port0.bit3 = Bit(5, 3)
+        self.port0.bit4 = Bit(6, 4)
+        self.port0.bit5 = Bit(7, 5)
 
-    port0.write(0x00)
-    port1.write(0x00)
-    while True:
-        for port in [port0, port1]:
-            for i in range(0x3F + 1):
-                port.write(i)
-                time.sleep(0.1)
+        self.port1 = Port(bus, ioexpander_address)
+        self.port1.read_address = P1_REG
+        self.port1.mask = 2
+
+        self.cross_points = {
+            1: {  # ROW
+                1: (self.port1, 0),  # COL
+                2: (self.port1, 2),
+                3: (self.port1, 4),
+                4: (self.port0, 1),
+                5: (self.port0, 3),
+                6: (self.port0, 5),
+            },
+            2: {  # ROW
+                1: (self.port1, 1),  # COL
+                2: (self.port1, 3),
+                3: (self.port1, 5),
+                4: (self.port0, 4),
+                5: (self.port0, 2),
+                6: (self.port0, 0),
+            }
+        }
+
+    def connect(self, row, column):
+        port, bit = self.cross_points[row][column]
+        port.set_bit(bit, 1)
+        port.send()
+
+    def disconnect(self, row, column):
+        port, bit = self.cross_points[row][column]
+        port.set_bit(bit, 0)
+        port.send()
+
+    def disconnect_all(self):
+        self.port0.write(0x00)
+        self.port1.write(0x00)
+
+
+def main():
+    with SMBus(1) as bus:
+        mux = Mux(bus, ioexpander_address)
+        for row in [1, 2]:
+            for col in [1, 2, 3, 4, 5, 6]:
+                print("connect", row, col)
+                mux.connect(row, col)
+                time.sleep(0.5)
+                print("disconnect", row, col)
+                mux.disconnect(row, col)
+                time.sleep(0.5)
+
+
+if __name__ == '__main__':
+    main()
